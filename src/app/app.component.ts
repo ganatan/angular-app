2801c668a0410b7cbeeb5c593bb56c8cd3a1eaf1
ganatan <dannyganatan@gmail.com>
2020-04-24 08:40:03 +0200

    Update Angular 9.1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-starter';
-  version = 'Angular version 9.1.2';
+  version = 'Angular version 9.1.3';
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 9.1.2',
+    angular: 'Angular 9.1.3',
     bootstrap: 'Bootstrap 4.4.1',
     fontawesome: 'Font Awesome 5.13.0',
   }
