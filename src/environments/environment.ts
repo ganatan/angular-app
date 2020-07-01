13e8b6c1809a8251a4f66a3c216eab6354cf0813
ganatan <dannyganatan@gmail.com>
2020-07-01 06:55:53 +0200

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 10.0.1',
+    angular: 'Angular 10.0.2',
     bootstrap: 'Bootstrap 4.5.0',
     fontawesome: 'Font Awesome 5.13.1',
   }
