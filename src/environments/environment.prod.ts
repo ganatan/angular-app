b0bf722d712b5258ee2561700a402764c85da5f5
ganatan <dannyganatan@gmail.com>
2020-09-15 16:16:03 +0200

    Update Home & e2e tests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   {
     name: 'angular-starter',
     angular: 'Angular 10.1.1',
-    bootstrap: 'Bootstrap 5.0.0-alpha1',
+    bootstrap: 'Bootstrap 5.0.0',
     fontawesome: 'Font Awesome 5.14.0',
   }
 };
