9eb008cecd76cc9e3548c8600197d201d47aac4a
ganatan <dannyganatan@gmail.com>
2020-11-24 11:48:59 +0100

    Add MySQL Manual SQL Scripts

---
+++ backend/mysql-sql/delete-tables.sql
@@ -0,0 +1,16 @@
+DROP TABLE users;
+DROP TABLE continent;
+DROP TABLE country;
+DROP TABLE city;
+DROP TABLE gender;
+DROP TABLE profession;
+DROP TABLE genre;
+DROP TABLE movie;
+DROP TABLE images;
+DROP TABLE person;
+DROP TABLE views;
+DROP TABLE trailer;
+DROP TABLE movie_person;
+DROP TABLE movie_genre;
+DROP TABLE movie_image;
+
