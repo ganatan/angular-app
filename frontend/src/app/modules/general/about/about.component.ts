b2737a1600736bd29f1098854f0be93687944856
ganatan <dannyganatan@gmail.com>
2021-02-04 07:16:21 +0100

    Update Angular 11.1.2 & Backend package.json

--- frontend/src/app/modules/general/about/about.component.ts
+++ frontend/src/app/modules/general/about/about.component.ts
@@ -13,9 +13,9 @@ export class AboutComponent {
   constructor() {
     this.dependencies = {
       frontend: [
-        { name: 'Angular 11.1.1' },
+        { name: 'Angular 11.1.2' },
         { name: 'Angular CLI 11.1.2' },
-        { name: 'Angular Universal 11.1.0' },
+        { name: 'Angular Universal 11.1.1' },
         { name: 'Font Awesome 5.15.2' },
         { name: 'Bootstrap 5.0.0-beta1' },
       ],
