486546ffd1450106030ac5ece77a1ae8c09ff169
ganatan <dannyganatan@gmail.com>
2021-04-24 07:11:26 +0200

    Update Angular 11.2.11

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 11.2.10',
+    angular: 'Angular 11.2.11',
     bootstrap: 'Bootstrap 5.0.0',
     fontawesome: 'Font Awesome 5.15.3',
   },
