f28a8705fe1b3d7328dfda651d96f300c249109c
ganatan <dannyganatan@gmail.com>
2021-05-16 17:53:09 +0200

    environment & index

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 12.0.10',
+    angular: 'Angular 12.0.0',
     bootstrap: 'Bootstrap 5.0.1',
     fontawesome: 'Font Awesome 5.15.3',
   }
