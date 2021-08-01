016f421e28c656b7a7126bc51e0dc92b2e50c54a
ganatan <dannyganatan@gmail.com>
2021-08-01 11:34:24 +0200

    Update Angular 12.1.4

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 12.1.3',
+    angular: 'Angular 12.1.4',
     bootstrap: 'Bootstrap 5.0.2',
     fontawesome: 'Font Awesome 5.15.3',
   }
