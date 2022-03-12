82e5906dd5d3248b3369ca6fb926f9c005f09bdb
ganatan <dannyganatan@gmail.com>
2022-03-12 11:14:46 +0100

    Update Angular 13.2.6

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-starter';
-  version = 'Angular version 13.2.5';
+  version = 'Angular version 13.2.6';
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 13.2.5',
+    angular: 'Angular 13.2.6',
     bootstrap: 'Bootstrap 5.1.3',
     fontawesome: 'Font Awesome 6.0.0',
   },
