b9214d32ba9a505225fa7113e220cb795b4bc7fc
ganatan <dannyganatan@gmail.com>
2022-05-19 14:12:59 +0200

    Update Angular 13.3.9

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-starter';
-  version = 'Angular version 13.3.8';
+  version = 'Angular version 13.3.9';
 }
