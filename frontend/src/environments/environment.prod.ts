3011f6f0e7a1ead240f266ea210e7fb03aefdad4
ganatan <dannyganatan@gmail.com>
2022-09-15 09:49:01 +0200

    Update Angular 14.2.2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 14.2.1',
+    angular: 'Angular 14.2.2',
     bootstrap: 'Bootstrap 5.2.1',
     fontawesome: 'Font Awesome 6.2.0',
   },
