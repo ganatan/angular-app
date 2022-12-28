0189999708f250f193c7c95982dedff6ee476a98
ganatan <dannyganatan@gmail.com>
2022-12-28 17:55:51 +0100

    Remove async on header component test

--- frontend/src/app/components/header/header.component.spec.ts
+++ frontend/src/app/components/header/header.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {  ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeaderComponent } from './header.component';
@@ -7,7 +7,7 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach((() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
