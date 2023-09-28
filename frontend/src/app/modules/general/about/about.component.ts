3af7c3f1c47d843dd67037a53bbebce9da1d80b4
ganatan <dannyganatan@gmail.com>
2023-09-28 09:42:27 +0200

    Update Angular 16.2.7 & Angular CLI 16.2.4

--- frontend/src/app/modules/general/about/about.component.ts
+++ frontend/src/app/modules/general/about/about.component.ts
@@ -30,8 +30,8 @@ export class AboutComponent implements OnInit {
     this.quote = new Quote();
     this.dependencies = {
       frontend: [
-        { name: 'Angular 16.2.6' },
-        { name: 'Angular CLI 16.2.3' },
+        { name: 'Angular 16.2.7' },
+        { name: 'Angular CLI 16.2.4' },
         { name: 'Angular Universal 16.2.0' },
         { name: 'Bootstrap 5.3.2' },
         { name: 'Font Awesome 6.4.2' },
