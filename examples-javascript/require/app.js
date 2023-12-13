a3f2df55f58122251c10762f9952f63e37531c8c
ganatan <dannyganatan@gmail.com>
2023-12-13 16:04:30 +0100

    Add require on examples-javascript

---
+++ examples-javascript/require/app.js
@@ -0,0 +1,6 @@
+const { display1, display2 } = require('./example')
+
+display1();
+display2();
+
+
---
+++ examples-javascript/require/example.js
@@ -0,0 +1,7 @@
+function display1() {
+  console.log('00000000001:display1:');
+}
+function display2() {
+  console.log('00000000001:display2:');
+}
+module.exports = { display1, display2 }
