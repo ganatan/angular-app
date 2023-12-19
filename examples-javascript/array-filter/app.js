0898ee8de23a1d1bc53238b81aa068b3a4dfbe8e
ganatan <dannyganatan@gmail.com>
2023-12-19 09:36:30 +0100

    Improve array.filter on examples-javascript

--- examples-javascript/array-filter/app.js
+++ examples-javascript/array-filter/app.js
@@ -61,7 +61,7 @@ function filterCondition(element) {
   condition = condition.toUpperCase();
   console.log('00000000001:' + title);
   console.log('00000000002:' + condition);
-    if (title.indexOf(condition) !== -1) {
+  if (title.indexOf(condition) !== -1) {
     console.log('00000000003:' + condition);
     res = true;
   };
@@ -69,9 +69,9 @@ function filterCondition(element) {
     console.log('00000000003:' + condition);
     res = true;
   };
-/*  if (title === condition) {
-    res = true;
-  }*/
+  /*  if (title === condition) {
+      res = true;
+    }*/
   return res;
 }
 let param = 'dune';
@@ -79,3 +79,23 @@ const results7 = items7.filter(filterCondition);
 
 console.log('results7: ');
 console.log('filter with uppercase: ' + JSON.stringify(results7));
+
+
+
+function filterCondition8(element) {
+  console.log('filterCondition8:' + JSON.stringify(element));
+  console.log('filterCondition8:' + typeof element.value);
+  let res = false;
+  if (element.value === 2 ) {
+    console.log('filterCondition8:true');
+    res = true;
+  };
+  return res;
+}
+
+const items8 = [
+  { "name": "- aucun -", "value": "- aucun -", "valueFieldReference": null },
+  { "value": 1, "name": "budget 160", "valueFieldReference": 1 },
+  { "value": 2, "name": "budget 225", "valueFieldReference": 2 }];
+const results8 = items8.filter(filterCondition8);  
+console.log('results8: ' + JSON.stringify(results8));
