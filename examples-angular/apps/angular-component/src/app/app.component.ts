078d3fe5534ced1d681b88644231feab42e7f2fd
ganatan <dannyganatan@gmail.com>
2024-02-19 17:39:43 +0100

    Update Angular 17.2.1

---
+++ examples-angular/apps/angular-component/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ImageComponent } from './image/image.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    ImageComponent,
+    RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'angular-component';
+}
