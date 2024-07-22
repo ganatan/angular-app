4c725783a4785eae1b2d08f5692f2c7861bb456e
ganatan <dannyganatan@gmail.com>
2024-07-22 10:44:58 +0200

    Update Angular 18.1.1

--- application-angular/src/environments/environment.development.ts
+++ application-angular/src/environments/environment.development.ts
@@ -2,9 +2,9 @@ export const environment = {
   application:
   {
     name: 'angular-starter',
-    angular: 'Angular 18.1.0 Dev',
+    angular: 'Angular 18.1.1 Dev',
     bootstrap: 'Bootstrap 5.3.3',
-    fontawesome: 'Font Awesome 6.5.2',
+    fontawesome: 'Font Awesome 6.6.0',
   },
   urlNews: './assets/params/json/mock/trailers.json',
   urlMovies: './assets/params/json/mock/movies.json',
