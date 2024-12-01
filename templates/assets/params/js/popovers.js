6cc4f0e8aa08a765a7f5c84c3723e412f5ea2202
ganatan <dannyganatan@gmail.com>
2024-12-01 10:38:19 +0100

    add bootstrap templates on frontend-angular

---
+++ templates/assets/params/js/popovers.js
@@ -0,0 +1,4 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
