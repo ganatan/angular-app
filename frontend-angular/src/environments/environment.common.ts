951697b4a72a4144e7df52c86fc0aeef333c8f67
ganatan <dannyganatan@gmail.com>
2024-12-08 08:06:09 +0100

    Add environment.common

---
+++ frontend-angular/src/environments/environment.common.ts
@@ -0,0 +1,17 @@
+export const appVersion = '241207-1136';
+
+export const appInfo = {
+  name: 'Ganatan',
+  nameUC: 'GANATAN',
+  logo: 'ganatan',
+  network: 'ganatan',
+  website: 'www.ganatan.com',
+};
+
+export const applicationBase = {
+  name: 'angular-starter',
+  angular: 'Angular 19.0.4',
+  bootstrap: 'Bootstrap 5.3.3',
+  fontawesome: 'Font Awesome 6.7.1',
+};
+
--- frontend-angular/src/environments/environment.development.ts
+++ frontend-angular/src/environments/environment.development.ts
@@ -1,17 +1,10 @@
+import { appInfo, applicationBase } from './environment.common';
+
 export const environment = {
-  appInfo: {
-    name: 'Ganatan',
-    nameUC: 'GANATAN',
-    logo: 'ganatan',
-    network: 'ganatan',
-    website: 'www.ganatan.com',
-  },
-  application:
-  {
-    name: 'angular-starter',
-    angular: 'Angular 19.0.4 Dev',
-    bootstrap: 'Bootstrap 5.3.3',
-    fontawesome: 'Font Awesome 6.7.1',
+  appInfo,
+  application: {
+    ...applicationBase,
+    angular: `${applicationBase.angular} DEV`,
   },
   urlNews: './assets/params/json/mock/trailers.json',
   urlMovies: './assets/params/json/mock/movies.json',
