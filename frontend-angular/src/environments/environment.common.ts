b24a5f515de97adcb451089910f46162dbf6012d
ganatan <dannyganatan@gmail.com>
2024-12-19 16:53:04 +0100

    Update Angular 19.0.5 & Angular CLI 19.0.6 on frontend-angular

--- frontend-angular/src/environments/environment.common.ts
+++ frontend-angular/src/environments/environment.common.ts
@@ -10,7 +10,7 @@ export const appInfo = {
 
 export const applicationBase = {
   name: 'angular-starter',
-  angular: 'Angular 19.0.4',
+  angular: 'Angular 19.0.5',
   bootstrap: 'Bootstrap 5.3.3',
   fontawesome: 'Font Awesome 6.7.2',
 };
