c549510e20b3586dd8345b9b018316abae24db95
ganatan <dannyganatan@gmail.com>
2025-01-12 07:12:55 +0100

    remove useless code

--- library-angular/projects/ganatan-angular/nga-edit/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of nga-edit
- */
-
-export * from './lib/nga-edit.service';
-export * from './lib/nga-edit.component';
--- library-angular/projects/ganatan-angular/nga-table/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of nga-table
- */
-
-export * from './lib/nga-table.service';
-export * from './lib/nga-table.component';
