a6c87cc444a766d2fc0b724a31b85e22a9817472
ganatan <dannyganatan@gmail.com>
2025-03-05 09:26:08 +0100

    Update Angular 19.2.0 on frontend-angular

--- frontend-angular/src/environments/environment.common.ts
+++ frontend-angular/src/environments/environment.common.ts
@@ -10,7 +10,7 @@ export const appInfo = {
 
 export const applicationBase = {
   name: 'angular-starter',
-  angular: 'Angular 19.1.4',
+  angular: 'Angular 19.2.0',
   bootstrap: 'Bootstrap 5.3.3',
   fontawesome: 'Font Awesome 6.7.2',
 };
