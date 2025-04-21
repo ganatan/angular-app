16f5ba8cc87fb71417720beb3235c3ed577e4987
ganatan <dannyganatan@gmail.com>
2025-04-21 07:41:31 +0200

    Add starters architecture

---
+++ starters/javascript/nodejs/commonjs/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const persons = [
+  { id: 1, name: 'Christopher Nolan' },
+  { id: 2, name: 'Quentin Tarantino' },
+  { id: 3, name: 'Steven Spielberg' },
+  { id: 4, name: 'Martin Scorsese' },
+  { id: 5, name: 'James Cameron' },
+  { id: 6, name: 'Ridley Scott' },
+  { id: 7, name: 'Denis Villeneuve' }
+]
+
+app.get('/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.listen(port, () => {
+  console.log(`API listening on http://localhost:${port}`)
+})
