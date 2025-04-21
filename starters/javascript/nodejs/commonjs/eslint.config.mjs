a6101367305b77472689baa5fb48c2357a7fc37b
ganatan <dannyganatan@gmail.com>
2025-04-21 08:04:01 +0200

    Add eslint on starter

---
+++ starters/javascript/nodejs/commonjs/eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js"
+import globals from "globals"
+import { defineConfig } from "eslint/config"
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "no-var": "error", 
+      "prefer-const": "warn", 
+      "no-unused-vars": "warn" 
+    }
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
+    }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.node
+    }
+  }
+])
