529e5d19bdcf091dadfb0b5f94ecd671165ccddc
ganatan <dannyganatan@gmail.com>
2025-04-21 09:11:41 +0200

    Change structure

--- starters/backend-javascript-commonjs/src/server.js
+++ starters/backend-javascript-commonjs/src/server.js
@@ -6,11 +6,3 @@ const server = app.listen(port, () => {
 })
 
 module.exports = server
-
-
-// const app = require('./app')
-// const port = 3000
-
-// app.listen(port, () => {
-//   console.log(`API listening on http://localhost:${port}`)
-// })
