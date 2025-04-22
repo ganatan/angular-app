1acdbba25a155aaf007808660ce071cc785b95e9
ganatan <dannyganatan@gmail.com>
2025-04-22 10:47:42 +0200

    Add backend-java21-spring

---
+++ starters/backend-java21-spring/src/main/java/com/example/controller/PersonController.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import com.example.model.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class PersonController {
+
+    @GetMapping("/api/persons")
+    @ResponseBody
+    public List<Person> getPersons() {
+        return List.of(
+            new Person("Christopher Nolan"),
+            new Person("Quentin Tarantino"),
+            new Person("Steven Spielberg"),
+            new Person("Martin Scorsese"),
+            new Person("Denis Villeneuve"),
+            new Person("James Cameron"),
+            new Person("Ridley Scott")
+        );
+    }
+}
