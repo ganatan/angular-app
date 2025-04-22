da7fd95d7a790eaddc29970a5585ab843181aafa
ganatan <dannyganatan@gmail.com>
2025-04-22 17:16:35 +0200

    Improve backend-java21-springboot

--- starters/backend-java21-springboot/src/main/java/com/example/controller/RootController.java
+++ starters/backend-java21-springboot/src/main/java/com/example/controller/RootController.java
@@ -32,21 +32,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+
 
 @RestController
-public class PersonController {
+public class RootController {
 
-    @GetMapping("/persons")
-    public List<Person> getAllPersons() {
-    	System.out.println("00000000001:PersonController");
-        return List.of(
-            new Person("Christopher Nolan"),
-            new Person("Quentin Tarantino"),
-            new Person("Steven Spielberg"),
-            new Person("Martin Scorsese"),
-            new Person("Denis Villeneuve"),
-            new Person("James Cameron"),
-            new Person("Ridley Scott")
-        );
-    }
+    @GetMapping("/")
+    
+  public Map<String, Object> getAppInfo() {
+  System.out.println("00000000001:PersonController");
+	  return Map.of(
+	      "success", true,
+	      "data", Map.of(
+	          "version", "1.0.0",
+	          "status", "ok",
+	          "app", "backend-java21-springboot"
+	      )
+	  );
+	}
+
+    
+//    public List<Person> getAllPersons() {
+//    	System.out.println("00000000001:PersonController");
+//        return List.of(
+//            new Person("Christopher Nolan Continent"),
+//            new Person("Quentin Tarantino"),
+//            new Person("Steven Spielberg"),
+//            new Person("Martin Scorsese"),
+//            new Person("Denis Villeneuve"),
+//            new Person("James Cameron"),
+//            new Person("Ridley Scott")
+//        );
+//    }
 }
