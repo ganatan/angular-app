c3840e39bffcd6c8e13a7780bb4194be52b11646
ganatan <dannyganatan@gmail.com>
2025-04-23 18:38:39 +0200

    Add swagger on backend-javascript-commonjs

--- starters/backend-javascript-commonjs/src/app.js
+++ starters/backend-javascript-commonjs/src/app.js
@@ -4,12 +4,16 @@ const express = require('express');
 
 const configureSecurity = require('./infrastructure/middleware/security/security.js');
 
+const swaggerRoutes = require('./infrastructure/swagger/swagger.routes');
+
 const appRoutes = require('./routers/app.routes.js');
 const rootRoutes = require('./routers/root.routes.js');
 
 const app = express();
 
 configureSecurity(app);
+
+app.use('/api-docs', swaggerRoutes);
 
 app.use(appRoutes);
 app.use(rootRoutes);
