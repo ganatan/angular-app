bee1bf8ce640324822a446998dd5321781a76759
ganatan <dannyganatan@gmail.com>
2025-04-26 10:33:53 +0200

    fix linter on backend-javascript

--- backend-javascript/__tests__/integration/server.test.js
+++ backend-javascript/__tests__/integration/server.test.js
@@ -3,10 +3,10 @@ import server from '../../src/server.js';
 describe('Server startup', () => {
   test('should start and listen correctly', async () => {
     // Arrange
-    const runningServer = server;
+    // const runningServer = server;
 
     // Act
-    const isListening = runningServer.listening;
+    // const isListening = runningServer.listening;
 
     // Assert
     expect(true).toBe(true);
