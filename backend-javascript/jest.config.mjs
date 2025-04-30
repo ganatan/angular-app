495529a97fd226e843b169191d7c9f6b8007ec6c
ganatan <dannyganatan@gmail.com>
2025-04-30 17:39:19 +0200

    fix lint error on backend-javascript

--- backend-javascript/jest.config.mjs
+++ backend-javascript/jest.config.mjs
@@ -18,25 +18,3 @@ const config = {
 };
 
 export default config;
-
-
-
-// export default {
-//   testEnvironment: 'node',
-//   testMatch: ['**/*.test.js'],
-//   moduleFileExtensions: ['js', 'json'],
-//   clearMocks: true,
-//   verbose: true,
-//   collectCoverage: false,
-//   collectCoverageFrom: [
-//     'src/**/*.{js,jsx}',
-//     '!src/**/__tests__/**',
-//     '!src/**/mocks/**',
-//     '!**/node_modules/**',
-//     '!**/vendor/**',
-//   ],
-//   coverageDirectory: 'coverage',
-//   coverageReporters: ['text', 'lcov', 'json-summary'],
-//   transform: {},
-//   extensionsToTreatAsEsm: ['.js'],
-// };
