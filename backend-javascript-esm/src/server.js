1bda1bfd5f5df34733291f9b91476a8c46f324f8
ganatan <dannyganatan@gmail.com>
2025-05-01 16:13:38 +0200

    Add port tests on backend-javascript-esm

--- backend-javascript-esm/src/server.js
+++ backend-javascript-esm/src/server.js
@@ -2,7 +2,17 @@ import app from './app.js';
 import appConfig from './config/app.config.js';
 
 const server = app.listen(appConfig.app.port, () => {
-  console.log(`API listening on http://localhost:${appConfig.app.port}`);
+  console.log(`✅ API listening on http://localhost:${appConfig.app.port}`);
+});
+
+server.on('error', (error) => {
+  if (error.code === 'EADDRINUSE') {
+    console.error(`❌ Port ${appConfig.app.port} is already in use.`);
+    process.exit(1);
+  } else {
+    console.error('❌ Unexpected server error:', error);
+    process.exit(1);
+  }
 });
 
 export default server;
