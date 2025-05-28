1c06079e39578d1e0641c42ea7992ba72d41498e
ganatan <dannyganatan@gmail.com>
2025-05-28 17:37:31 +0200

    Update Angular 19.2.14

--- frontend-angular/src/app/modules/general/about/about.component.ts
+++ frontend-angular/src/app/modules/general/about/about.component.ts
@@ -51,9 +51,9 @@ export class AboutComponent implements OnInit {
     this.quote = new Quote();
     this.dependencies = {
       frontend: [
-        { name: 'Angular 19.2.13' },
-        { name: 'Angular CLI 19.2.13' },
-        { name: 'Angular SSR 19.2.13' },
+        { name: 'Angular 19.2.14' },
+        { name: 'Angular CLI 19.2.14' },
+        { name: 'Angular SSR 19.2.14' },
         { name: 'Bootstrap 5.3.6' },
         { name: 'Font Awesome 6.7.2' },
       ],
