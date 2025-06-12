df811e891f4858ccdb6d99417fdc210130fccb28
ganatan <dannyganatan@gmail.com>
2025-06-12 09:30:07 +0200

    Update Angular 20.0.3 & Angular CLI 20.0.2

--- frontend-angular/src/environments/environment.common.ts
+++ frontend-angular/src/environments/environment.common.ts
@@ -12,7 +12,7 @@ export const appInfo = {
 
 export const applicationBase = {
   name: 'angular-starter',
-  angular: 'Angular 20.0.2',
+  angular: 'Angular 20.0.3',
   bootstrap: 'Bootstrap 5.3.6',
   fontawesome: 'Font Awesome 6.7.2',
 };
