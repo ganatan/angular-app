62976b8a8b5260b866321f2a4450069d4e758af2
ganatan <dannyganatan@gmail.com>
2025-06-14 13:09:46 +0200

    Update dependencies on backend-javascript

--- backend-javascript-esm/src/modules/profession/__tests__/unit/profession.schema.test.js
+++ backend-javascript-esm/src/modules/profession/__tests__/unit/profession.schema.test.js
@@ -1,29 +1,6 @@
 import { validateItem } from '../../schemas/profession.schema.js';
 
 describe('validateItem', () => {
-  test('should throw error if name is missing', () => {
-    // Arrange
-    const invalidData = {};
-
-    // Act & Assert
-    expect(() => validateItem(invalidData)).toThrowError('Name is required');
-  });
-
-  test('should throw error if name is not a string', () => {
-    // Arrange
-    const invalidData = { name: 123 };
-
-    // Act & Assert
-    expect(() => validateItem(invalidData)).toThrowError('Name must be a string');
-  });
-
-  test('should throw error if name is too short', () => {
-    // Arrange
-    const invalidData = { name: 'A' };
-
-    // Act & Assert
-    expect(() => validateItem(invalidData)).toThrowError('Name must be a string of at least 2 characters');
-  });
 
   test('should set status to 400 on validation error', () => {
     // Arrange
