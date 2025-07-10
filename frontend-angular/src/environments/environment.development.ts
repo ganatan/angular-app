1709247892a0af0827fe59edfa734b010b557e84
ganatan <dannyganatan@gmail.com>
2025-07-10 15:13:41 +0200

    fix npm install on backend-javascript-cjs

--- frontend-angular/src/environments/environment.development.ts
+++ frontend-angular/src/environments/environment.development.ts
@@ -8,6 +8,6 @@ export const environment = {
   },
   urlNews: './assets/params/json/mock/trailers.json',
   urlMovies: './assets/params/json/mock/movies.json',
-  useDatabase: false,
+  useDatabase: true,
   backend: 'http://localhost:3000',
 };
