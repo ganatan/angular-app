6a3bbab555bcc0efac6b34ce2b4bcf925799ce60
ganatan <dannyganatan@gmail.com>
2025-07-17 16:44:56 +0200

    improve bind on backend-javascript

--- backend-javascript/src/modules/person/person.controller.js
+++ backend-javascript/src/modules/person/person.controller.js
@@ -5,14 +5,9 @@ import { validateItem } from './person.schema.js';
 class Controller {
   constructor(service) {
     this.service = service;
-    this.getItems = this.getItems.bind(this);
-    this.getItemById = this.getItemById.bind(this);
-    this.createItem = this.createItem.bind(this);
-    this.updateItem = this.updateItem.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
   }
 
-  async getItems(req, res, next) {
+  getItems = async (req, res, next) => {
     try {
       const result = await this.service.getItems(req.query);
       res.locals = { data: result, statusCode: HTTP_STATUS.OK };
@@ -22,9 +17,9 @@ class Controller {
 
       return next(error);
     }
-  }
+  };
 
-  async getItemById(req, res, next) {
+  getItemById = async (req, res, next) => {
     try {
       const result = await this.service.getItemById(parseInt(req.params.id));
       res.locals = { data: result, statusCode: HTTP_STATUS.OK };
@@ -46,9 +41,9 @@ class Controller {
 
       return next(error);
     }
-  }
+  };
 
-  async createItem(req, res, next) {
+  createItem = async (req, res, next) => {
     try {
       validateItem(req.body);
       const result = await this.service.createItem(req.body);
@@ -65,9 +60,9 @@ class Controller {
 
       return next(error);
     }
-  }
+  };
 
-  async updateItem(req, res, next) {
+  updateItem = async (req, res, next) => {
     try {
       validateItem(req.body);
       const result = await this.service.updateItem(parseInt(req.params.id), req.body);
@@ -84,9 +79,9 @@ class Controller {
 
       return next(error);
     }
-  }
+  };
 
-  async deleteItem(req, res, next) {
+  deleteItem = async (req, res, next) => {
     try {
       const result = await this.service.deleteItem(parseInt(req.params.id));
       res.locals = { data: result, statusCode: HTTP_STATUS.OK };
@@ -99,7 +94,7 @@ class Controller {
 
       return next(error);
     }
-  }
+  };
 }
 
 export default Controller;
