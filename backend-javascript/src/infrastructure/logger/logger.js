72c80e9f654b917c4eeaa20f2193de14206ce9d3
ganatan <dannyganatan@gmail.com>
2025-07-19 07:27:52 +0200

    Test elastisearch & logstash on backend-javascript

--- backend-javascript/src/infrastructure/logger/logger.js
+++ backend-javascript/src/infrastructure/logger/logger.js
@@ -2,13 +2,47 @@ import { createLogger, transports, format } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 import { ElasticsearchTransport } from 'winston-elasticsearch';
 
-const esTransport = new ElasticsearchTransport({
-  level: 'info',
-  indexPrefix: 'backend-javascript-logs',
-  clientOpts: {
-    node: process.env.ELASTICSEARCH_NODE || 'http://localhost:9200',
-  },
-});
+const transportsArray = [];
+
+if (process.env.LOGSTASH_ENABLED === 'true') {
+  transportsArray.push(
+    new DailyRotateFile({
+      filename: 'logs/logstash-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      level: 'info',
+      zippedArchive: true,
+      maxSize: '10m',
+      maxFiles: '30d',
+    }),
+  );
+} else if (process.env.ELASTIC_ENABLED === 'true') {
+  const esTransport = new ElasticsearchTransport({
+    level: 'info',
+    indexPrefix: 'backend-javascript-logs',
+    clientOpts: {
+      node: process.env.ELASTICSEARCH_NODE || 'http://localhost:9200',
+    },
+  });
+  transportsArray.push(esTransport);
+}
+
+transportsArray.push(
+  new DailyRotateFile({
+    filename: 'logs/error-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    level: 'error',
+    zippedArchive: true,
+    maxSize: '5m',
+    maxFiles: '120d',
+  }),
+  new DailyRotateFile({
+    filename: 'logs/combined-%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '5m',
+    maxFiles: '120d',
+  }),
+);
 
 const logger = createLogger({
   level: process.env.LOG_LEVEL || 'warn',
@@ -18,24 +52,7 @@ const logger = createLogger({
     format.splat(),
     format.json(),
   ),
-  transports: [
-    esTransport,
-    new DailyRotateFile({
-      filename: 'logs/error-%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      level: 'error',
-      zippedArchive: true,
-      maxSize: '5m',
-      maxFiles: '120d',
-    }),
-    new DailyRotateFile({
-      filename: 'logs/combined-%DATE%.log',
-      datePattern: 'YYYY-MM-DD',
-      zippedArchive: true,
-      maxSize: '5m',
-      maxFiles: '120d',
-    }),
-  ],
+  transports: transportsArray,
 });
 
 if (process.env.NODE_ENV !== 'production') {
@@ -47,3 +64,4 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 export default logger;
+
